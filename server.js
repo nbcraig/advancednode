75feb63e0a8823f85207fad9bb9348d1ee8a85cc
Beni Craig Ndagano <ibenicraig@gmail.com>
2023-04-07 23:20:57 +0300

    setting up Express

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+//const pug = require('pug');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index.pug')
 });
 
 const PORT = process.env.PORT || 3000;
