7109093cf8714eab9ee99c4a419975954d83e119
Beni Craig Ndagano <ibenicraig@gmail.com>
2023-04-11 22:27:46 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -26,7 +26,7 @@ module.exports = function (app, myDataBase) {
     });
   }));
 
-  passport.use(new GitHubStrategy({
+  passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: 'https://boilerplate-advancednode.sky020.repl.co/auth/github/callback'
