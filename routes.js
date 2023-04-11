3a8d0d7ffe4003e28f8fff6d746bc24ff257996b
Beni Craig Ndagano <ibenicraig@gmail.com>
2023-04-11 20:37:05 +0300

    Auth with Socket.io

--- routes.js
+++ routes.js
@@ -61,7 +61,13 @@ module.exports = function (app, myDataBase) {
 
   app.route('/auth/github/callback')
     .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-      res.redirect('/profile');
+        req.session.user_id = req.user.id
+        res.redirect('/chat');
+    })
+
+  app.route('/chat')
+    .get(ensureAuthenticated, (req, res) => {
+        res.render('/chat.pug', { user: req.user })
     })
 
   app.use((req, res, next) => {
